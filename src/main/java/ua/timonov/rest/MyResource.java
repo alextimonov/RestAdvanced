5e16f2d6fe3a9d951e48de9737ec53f682bd1cf5
Alex Timonov <timalex1206@gmail.com>
2016-12-30 20:59:00 +0200

    unit 3 - param annotations

--- src/main/java/ua/timonov/rest/MyResource.java
+++ src/main/java/ua/timonov/rest/MyResource.java
@@ -1,20 +1,20 @@
 package ua.timonov.rest;
 
-import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-@Singleton
+@Path("{pathParam}/test")
+//@Singleton
 public class MyResource {
     private int count;
+    @PathParam("pathParam") private String pathParamExample;
+    @QueryParam("query") private String queryParamExample;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String testMethod() {
         count++;
-        return "It works! This method was called " + count + " time(s)";
+        return "It works! This method was called " + count + " time(s). Path param used " + pathParamExample +
+                " and Query param used " + queryParamExample;
     }
 }
