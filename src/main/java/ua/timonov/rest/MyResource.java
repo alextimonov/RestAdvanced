dd90edaa0e4b465f9f3a03447928d9e5cb45921b
Alex Timonov <timalex1206@gmail.com>
2017-01-01 21:10:22 +0200

    unit 5 - MessageBodyWriter impl

--- src/main/java/ua/timonov/rest/MyResource.java
+++ src/main/java/ua/timonov/rest/MyResource.java
@@ -2,11 +2,14 @@ package ua.timonov.rest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Calendar;
+import java.util.Date;
 
-@Path("{pathParam}/test")
+@Path("test")
+//@Path("{pathParam}/test")
 //@Singleton
 public class MyResource {
-    private int count;
+    /*private int count;
     @PathParam("pathParam") private String pathParamExample;
     @QueryParam("query") private String queryParamExample;
 
@@ -16,5 +19,11 @@ public class MyResource {
         count++;
         return "It works! This method was called " + count + " time(s). Path param used " + pathParamExample +
                 " and Query param used " + queryParamExample;
+    }*/
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Date testMethod() {
+        return Calendar.getInstance().getTime();
     }
 }
